f47e538f717f15ed4621c7515b3932ed0d925c8d
Darya <135708765+DaryaDmitrievna02@users.noreply.github.com>
2024-08-18 21:10:18 +0300

    Update auth key

--- src/data/options.ts
+++ src/data/options.ts
@@ -3,7 +3,7 @@ export const options = {
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMmU2MGJiZDUwMzY0YjVlNzM1NThmMWYzZGU3NmY1YSIsIm5iZiI6MTcyMzExMTMwNi42NTM1NjYsInN1YiI6IjY2YjQ5NjNmODg5N2EzMGQzZjY0ODY4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BzLVnr2-aYdWnnn1GT3xEFQpml4kbdVAa5IorP_j_Jk",
+      "#KEY",
   },
 };
 
